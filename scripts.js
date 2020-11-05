b1336ea22588eb7c18ad6065cdf5d3d03c70030b
SweDark <jesper-jeppe@hotmail.com>
2020-11-05 22:21:29 +0100

    html code and css update

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+const paybutton = document.getElementById("submit-payment");
+paybutton.addEventListener("click", onClickPay);
+
+function onClickPay(){
+    alert("Du är nu en nöjd ägare av ett par köttpåsar!");
+}
